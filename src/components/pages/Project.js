c105f2959208a952d34f46b0bd217dc82390116a
Natalie Bulger <bulgern20@gmail.com>
2022-07-16 20:26:45 -0700

    Attempting a react grid

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import GridLayout from "react-grid-layout";
 
 const styles = {
   projectImage: {
@@ -26,41 +27,56 @@ const styles = {
 };
 
 export default function Project(props) {
+  const layout = [
+    { i: "1", x: 0, y: 0, w: 10, h: 10 },
+    { i: "2", x: 10, y: 0, w: 2, h: 5 },
+    { i: "3", x: 0, y: 10, w: 2, h: 5 },
+    { i: "4", x: 5, y: 10, w: 2, h: 5 },
+    { i: "5", x: 10, y: 10, w: 2, h: 5 },
+  ];
   return (
     <div className="container">
       <article className="work" id="work" style={styles.work}>
-        {props.projects.map((item) => (
-          <section key={item.id}>
-            <figure>
-              <div className="card-text">
-                <h3 className="card-header">{item.title}</h3>
-                <img
-                  className="project-image"
-                  style={styles.projectImage}
-                  src={item.project_image}
-                ></img>
-                <a
-                  className="card-body-link"
-                  href={item.deployed_link}
-                  target="_blank"
-                  rel="noreferrer"
-                  style={styles.workLink}
-                >
-                  Link to Deployed Project
-                </a>
-                <a
-                  className="card-body-link"
-                  href={item.git_link}
-                  target="_blank"
-                  rel="noreferrer"
-                  style={styles.workLink}
-                >
-                  Link to Github Repository
-                </a>
-              </div>
-            </figure>
-          </section>
-        ))}
+        <GridLayout
+          className="layout"
+          layout={layout}
+          cols={12}
+          rowHeight={30}
+          width={1200}
+        >
+          {props.projects.map((item) => (
+            <section key={item.id}>
+              <figure>
+                <div className="card-text">
+                  <h3 className="card-header">{item.title}</h3>
+                  <img
+                    className="project-image"
+                    style={styles.projectImage}
+                    src={item.project_image}
+                  ></img>
+                  <a
+                    className="card-body-link"
+                    href={item.deployed_link}
+                    target="_blank"
+                    rel="noreferrer"
+                    style={styles.workLink}
+                  >
+                    Link to Deployed Project
+                  </a>
+                  <a
+                    className="card-body-link"
+                    href={item.git_link}
+                    target="_blank"
+                    rel="noreferrer"
+                    style={styles.workLink}
+                  >
+                    Link to Github Repository
+                  </a>
+                </div>
+              </figure>
+            </section>
+          ))}
+        </GridLayout>
       </article>
     </div>
   );
