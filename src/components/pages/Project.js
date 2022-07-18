dfb158b5c93180437da9c9ff114edcb7fdc237b7
Natalie Bulger <bulgern20@gmail.com>
2022-07-18 15:23:56 -0700

    Progress on projects component

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 
 const styles = {
+  projectImage: {
+    maxWidth: "300px",
+    padding: "5px",
+    height: "300px",
+    objectFit: "cover",
+    borderRadius: "25px",
+  },
   work: {
     display: "grid",
     gridTemplateColumns: "2fr 2fr 2fr",
-    gridTemplateRows: "200px 200px",
+    gridTemplateRows: "auto",
     gap: "10px",
     gridAutoRows: "minmax(max-content, 2fr)",
     width: "100%",
@@ -16,6 +23,12 @@ const styles = {
     color: "black",
     display: "block",
   },
+
+  cardFormat: {
+    backgroundColor: "var(--offwhite)",
+    borderRadius: "25px",
+    textAlign: "center",
+  },
 };
 
 export default function Project(props) {
@@ -23,39 +36,30 @@ export default function Project(props) {
     <div className="container">
       <article className="work" id="work" style={styles.work}>
         {props.projects.map((item) => (
-          <section
-            key={item.id}
-            style={{
-              backgroundImage: `url(${item.project_image})`,
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              borderRadius: "25px",
-            }}
-          >
-            <figure>
-              <div className="card-text">
-                <h3 className="card-header">{item.title}</h3>
-                <a
-                  className="card-body-link"
-                  href={item.deployed_link}
-                  target="_blank"
-                  rel="noreferrer"
-                  style={styles.workLink}
-                >
-                  Link to Deployed Project
-                </a>
-                <a
-                  className="card-body-link"
-                  href={item.git_link}
-                  target="_blank"
-                  rel="noreferrer"
-                  style={styles.workLink}
-                >
-                  Link to Github Repository
-                </a>
-              </div>
-            </figure>
-          </section>
+          <div key={item.id}>
+            <div className="card-text" style={styles.cardFormat}>
+              <h3>{item.title}</h3>
+              <img src={item.project_image} style={styles.projectImage}></img>
+              <a
+                className="card-body-link"
+                href={item.deployed_link}
+                target="_blank"
+                rel="noreferrer"
+                style={styles.workLink}
+              >
+                Link to Deployed Project
+              </a>
+              <a
+                className="card-body-link"
+                href={item.git_link}
+                target="_blank"
+                rel="noreferrer"
+                style={styles.workLink}
+              >
+                Link to Github Repository
+              </a>
+            </div>
+          </div>
         ))}
       </article>
     </div>
